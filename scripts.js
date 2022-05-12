96f3730973c575f36395488f7a576bfc23d5c5cd
Daniel Lim <DanielLimWeiEn@gmail.com>
2022-05-12 13:44:27 +0800

    Create add, subtract, multiply and divide functions in scripts.js

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// 1. Basic Mathematical Functions.
+
+function add(first, second) {
+    return first + second;
+}
+
+function subtract(first, second) {
+    return first - second;
+}
+
+function multiply(first, second) {
+    return first * second;
+}
+
+function divide(first, second) {
+    return first / second;
+}
