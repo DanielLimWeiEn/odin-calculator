f2e1431e1bcbf2e20e92cedef3872d95a3fda2fb
Daniel Lim <DanielLimWeiEn@gmail.com>
2022-05-12 15:31:40 +0800

    Add event listeners to number buttons to update bottom display and update index.html

--- scripts.js
+++ scripts.js
@@ -33,5 +33,22 @@ function operate(first, operator, second) {
 }
 
 
+// 3. Make the buttons display the correct things upon clicking on them.
+const displayTop = document.querySelector(".displaytop");
+const displayBottom = document.querySelector(".displaybottom");
 
-// 4. Make the buttons 
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+
+const clear = document.querySelector(".clear");
+const equal = document.querySelector(".equal");
+
+numbers.forEach(numberBtn => {
+    numberBtn.addEventListener("click", event => {
+        let len = displayBottom.textContent.split("").length;
+        console.log(len);
+        if (len <= 25) {
+           displayBottom.textContent += numberBtn.textContent;
+        }
+    });
+});
