ef3f4d1bf4301ef0478a04236d652f4706de66aa
Daniel Lim <DanielLimWeiEn@gmail.com>
2022-05-12 13:50:34 +0800

    Create operate function that represents carrying out an operation on two operands using standard mathematical form

--- scripts.js
+++ scripts.js
@@ -15,3 +15,19 @@ function multiply(first, second) {
 function divide(first, second) {
     return first / second;
 }
+
+// 2. Operate Function.
+
+function operate(first, operator, second) {
+    if (operator === "+") {
+        return add(first, second);
+    } else if (operator === "-") {
+        return subtract(first, second);
+    } else if (operator === "*") {
+        return multiply(first, second);
+    } else if (operator === "/") {
+        return divide(first, second);
+    } else {
+        return;
+    }
+}
